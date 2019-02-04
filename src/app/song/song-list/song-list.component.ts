ddaa803da22238ef3a15ea5c238b00d34a21a39a
alex <alex.schechter@appcard.com>
2019-02-04 19:22:21 +0200

    remove unneccery code

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -14,28 +14,6 @@ import { SongService } from '../songs.service';
 })
 export class SongListComponent implements OnInit {
   selectedSong: Song;
-  // songs: Song[]= [
-  //   {
-  //     id: "1",
-  //     name: "Song1",
-  //     genre: Genre.BLUES,
-  //     song_path:"d",
-  //     image_path:"http://material.angular.io/assets/img/examples/shiba2.jpg",
-  //     release_date: new Date('2019-1-1'),
-  //     artists: ['Drake ,CardiB'],
-  //     num_of_times_liked: 23
-  //   },
-  //   {
-  //     id: "2",
-  //     name: "Song2",
-  //     genre: Genre.CLASSIC,
-  //     song_path:"d",
-  //     image_path: "http://r.ddmcdn.com/s_f/o_1/APL/uploads/2015/07/cecil-AP463227356214-1000x400.jpg",
-  //     release_date: new Date('2018-11-1'),
-  //     artists: ['Pink, PostyPost'],
-  //     num_of_times_liked: 9
-  //   }
-  // ];
   songs: Song[];
   private songSub: Subscription;
 
