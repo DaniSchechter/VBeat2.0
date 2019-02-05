13c18bce3072b83baccf9d386b0fd24e576a4e35
alex <alex.schechter@appcard.com>
2019-02-05 10:57:24 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SongListComponent } from './song/song-list/song-list.component';
+import { SongDetailsComponent } from './song/song-details/song-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SongListComponent},
+  {path: 'create', component: SongCreateComponent},
+  {path: 'details/:id', component: SongDetailsComponent},
+  {path: 'edit/:id', component: SongEditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
