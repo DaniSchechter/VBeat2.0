756f044716b1cc60f76575909e8a4c579a5cbc7c
GI <guyishay2@gmail.com>
2019-02-05 21:26:25 +0200

    writing user schema

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+	username: {type:String, require:true},
+	password: {type:String, require:true},
+	role: {type:Number,enum: ['User', 'Artist'] ,require:true},
+	profile_pic: {type: String, require: false},
+	display_name: {type:String, require: true},
+	email: {type:String, require:true},
+});
+
+module.exports = mongoose.model('User', userSchema);
