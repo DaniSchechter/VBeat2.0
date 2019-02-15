0dd4cdb500aa4aa1a4d4e02c3cb345b302f6c0fc
GI <guyishay2@gmail.com>
2019-02-15 16:40:36 +0200

    fixing user-login page

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private userService:UserService ) { }
 
   ngOnInit() {
   }
 
+  // handler for login
+  onSubmit(form: NgForm) {
+  	if(!form.valid) {
+  		return;
+  	}
+
+  	this.userService.login(form.value.username, form.value.password, this.handleSuccessfulLogin, this.handleFailedLogin);
+  }
+
+  handleSuccessfulLogin(){
+    alert('logged in!');
+  }
+
+  handleFailedLogin(){
+    alert('failed to login');
+  }
 }
