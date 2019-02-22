efff220a5b434203a85a4d8c3b86aaa4544efe76
GI <guyishay2@gmail.com>
2019-02-22 13:39:22 +0200

    socket.io compatibility with angular 6 & backend user agent validation

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -87,3 +87,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+ /*Fixing dani's shit*/
+ (window as any).global = window;
