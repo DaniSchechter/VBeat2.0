86fe66e08abaee81bdd801c5ee7f5a797c1ad5b7
alex <alex.schechter@appcard.com>
2019-02-24 21:33:09 +0200

    fix

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -81,20 +81,6 @@ app.get("", (req, res, next) => {
     });
 });
 
-// app.delete("/song/:id", (req, res, next) => {
-//     Playlist.deleteMany(songList._id = req.params.id)
-//     .then(result => {
-//         res.status(200).json({
-//             message : "Song deleted from playlists"
-//         });
-//     }).catch(error => {
-//         res.status(400).json({
-//             message: error.message
-//         });
-//     });
-// });
-
-
 // delete playlist
 app.delete("/:id", (req, res, next) => {
     Playlist.deleteOne({_id: req.params.id})
