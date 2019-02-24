8bfa69b3bb4a54a7c61771de5cc138c64384bc02
alex <alex.schechter@appcard.com>
2019-02-24 21:35:19 +0200

    remove console log

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -28,7 +28,6 @@ export class SongListComponent implements OnInit {
     this.songsService.getSongs(this.songsPerPage, 1);
     this.songSub = this.songsService.getSongsUpdateListener()
     .subscribe((songData: {songs: Song[], totalSongs: number}) => {
-      console.log("update");
       this.totalSongs = songData.totalSongs;
       this.songs = songData.songs;
       this.selectedSong = null;
