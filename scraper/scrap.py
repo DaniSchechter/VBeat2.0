8a3e9a095a3989c3eb66d261137769f6e0111dc4
GI <guyishay2@gmail.com>
2019-02-25 20:48:55 +0200

    name change

--- scraper/scrap.py
+++ scraper/scrap.py
@@ -148,7 +148,7 @@ class Song():
 		self.genre = random.choice(['POP', 'JAZZ', 'HIP-HOP', 'ROCK', 'CLASSIC'])  
 		self.release_date = release_date
 		self.song_path = song_url
-		self.album_image_path = 'https://www.thoughtco.com/thmb/oXOWMUKShSv3ym-a1xMVGPoabPM=/1200x800/filters:fill(auto,1)/archer_closeup-56a00f9f3df78cafda9fde1c.png' # just because
+		self.image_path = 'https://www.thoughtco.com/thmb/oXOWMUKShSv3ym-a1xMVGPoabPM=/1200x800/filters:fill(auto,1)/archer_closeup-56a00f9f3df78cafda9fde1c.png' # just because
 		self.scraped = True
 		self.num_of_times_liked = 0
 		#logger.info('initialized Song=%s' % self)	
