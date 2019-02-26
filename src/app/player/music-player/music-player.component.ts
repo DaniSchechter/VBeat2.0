ec0bddf0eb27d17abb7df88fbfd7356177b802ab
Dani <daniel.schechter@appcard.com>
2019-02-26 13:51:31 +0200

    player checks

--- src/app/player/music-player/music-player.component.ts
+++ src/app/player/music-player/music-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Song } from '../../song/song.model';
 
 @Component({
   selector: 'app-music-player',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./music-player.component.css']
 })
 export class MusicPlayerComponent implements OnInit {
+
+  songs: string[] = [
+    "../../../assets/songs/Alessia Cara - Scars To Your Beautiful (Audio).mp3",
+    "../../../assets/songs/Anne-Marie - 2002 [Official Video].mp3",
+    "../../../assets/songs/Clean Bandit - Symphony (feat. Zara Larsson) [Official Video].mp3",
+    "../../../assets/songs/Hailee Steinfeld, Alesso - Let Me Go ft. Florida Georgia Line, WATT.mp3",
+    "../../../assets/songs/Lauv - I Like Me Better [Official Audio].mp3",
+    "../../../assets/songs/Marshmello & Anne-Marie - FRIENDS (Music Video) OFFICIAL FRIENDZONE ANTHEM.mp3",
+    "../../../assets/songs/Marshmello ft. Bastille - Happier (Official Music Video).mp3",
+    "../../../assets/songs/Selena Gomez, Marshmello - Wolves (Official Music Video).mp3",
+  ];
 
   constructor() { }
 
