bcb9fe59da21368355a13f140cd54e7f6d9fc711
Dani <daniel.schechter@appcard.com>
2019-02-26 17:36:06 +0200

    user routes changes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,19 +18,9 @@ app.get("/logout", (req, res, next) => {
 	});
 });
 
+// Create a new user
 app.post("", (req, res, next) => {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password, 
-        role: req.body.role,
-        profile_pic: req.body.profile_pic,
-        display_name: req.body.display_name, 
-        email: req.body.email,
-        country: req.body.country,
-        city: req.body.city,
-        street: req.body.street,
-        houseNum: req.body.houseNum
-    });
+    const user = new User(req.body);
     user.save()
     .then(newUser => {
         req.session.userId = newUser._id;
@@ -45,18 +35,23 @@ app.post("", (req, res, next) => {
     });
 });
 
-// user login
+// User login
 app.post("/login", (req, res, next) => {
-    User.find({username: req.body.username, password: req.body.password})
+    User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    })
     .then(resultData => {
-        if(resultData != undefined && resultData.length == 1) {
-            req.session.userId = resultData[0]._id;
-	    // notify browser counter
-	    browserCounter.onLogin(req.headers['user-agent']);
+        if(resultData) {
 
             // create session
+            req.session.userId = resultData._id;
+
+            // notify browser counter
+            browserCounter.onLogin(req.headers['user-agent']);
+
             res.status(200).json({
-                userId: resultData[0]._id, 
+                userId: resultData._id, 
                 message: "Loged in successfully",
             });
 
@@ -70,7 +65,9 @@ app.post("/login", (req, res, next) => {
 
 // get all artists ( roll = "Artist" )
 app.get("/artists", (req, res, next) => {
-    User.find({ role: "ARTIST" })
+    User.find({ 
+        role: "ARTIST" 
+    })
     .then(artists => {
         if(artists == null || artists == undefined) {
             res.status(404).json({
@@ -90,54 +87,58 @@ app.get("/artists", (req, res, next) => {
 app.get("/currentUser", (req, res, next) => {
     if(!req.session.userId){
     	res.status(401).json({
-		message: 'not logged in'
-	});
-    	return;
+		    message: 'not logged in'
+	    });
     }
-    User.findOne({_id:req.session.userId})
-        .then(userResult => {
-            if(userResult){
-                res.status(200).json({
-                    user: userResult
-                });
-            }
-            else{
-                res.status(200).json({
-                    user: null
-                });
-            }
+
+    User.findOne({
+        _id:req.session.userId
+    })
+    .then(user => {
+        if(user){
+            res.status(200).json({
+                user: user
+            });
         }
-    ).catch(error => {
+        else{
+            res.status(200).json({
+                user: null
+            });
+        }
+    })
+    .catch(error => {
         res.status(400).json({
             message: "Coult not get the status of the user"
         });
     });
 })
 
-// get users
+// get all users
 app.get("", (req, res, next) => {
-    User.find(
-        ).then(userResult => {
-	userResult.forEach(user => user.password="<censored>");
+    User.find()
+    .then(users => {
+	    users.forEach(user => user.password="<censored>");
         res.status(200).json({
             message: "ok",
             users: userResult
         });
-    }).catch(error => {
+    })
+    .catch(error => {
         res.status(400).json({
             message: "Could not get all users"
         });
-    });;
+    });
 });
 
 // get certain user by id
 app.get("/:id", (req, res, next) => {
-    User.find(
-        ).then(userResult => {
-        if(userResult == null || userResult == undefined) {
+    User.findOne({
+        _id: req.params.id
+    })
+    .then(user => {
+        if(user == null || user == undefined) {
             res.status(404).json({
                 message: "not found",
-                users: []
             });
         } else {
             res.status(200).json({
@@ -150,25 +151,5 @@ app.get("/:id", (req, res, next) => {
         });
     });;
 });
-
-
-
-// delete certain user
-app.delete("/:id", (req, res, next) => {
-    User.deleteOne({_id: req.params.id})
-    .then(result => {
-        res.status(200).json({
-            message : "User deleted"
-        });
-    }).catch(error => {
-        console.log(error.message);
-        res.status(400).json({
-            message: "Could not delete user"
-        });
-    });
-});
-
-
-
 
 module.exports = app;
