e4a9df4453fd975191dce2730515df6366165f84
GI <guyishay2@gmail.com>
2019-02-27 19:31:30 +0200

    writing quick search song list

--- src/app/quick-search/quick-search.component.ts
+++ src/app/quick-search/quick-search.component.ts
@@ -10,7 +10,7 @@ export class QuickSearchComponent implements OnInit {
 		name: 'test',
 		genre: 'BLUES', 
 		song_path: 'http://none',
-		image_path: 'http://none',
+		image_path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyoxytt4ywnKXQJdgeZG5Z41Umk92jcWMGB7SGwg_rnO8KKYYP',
 		release_date: 'http://none',
 		artists:['some_object_id'],
 		num_of_times_liked: 1
@@ -19,7 +19,7 @@ export class QuickSearchComponent implements OnInit {
 		name: 'test2',
 		genre: 'CLASSIC', 
 		song_path: 'http://none',
-		image_path: 'http://none',
+		image_path: 'https://i.kinja-img.com/gawker-media/image/upload/s--_40CUoZX--/c_fill,f_auto,fl_progressive,g_center,h_264,q_80,w_470/i3x37tdozf9bpui6ssec.jpg',
 		release_date: 'http://none',
 		artists:['some_object_id'],
 		num_of_times_liked: 2
