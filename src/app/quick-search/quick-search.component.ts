8d645c5c8687e676180bdfae83ac5391d0bed9df
GI <guyishay2@gmail.com>
2019-02-27 19:58:48 +0200

    search songs on server

--- src/app/quick-search/quick-search.component.ts
+++ src/app/quick-search/quick-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SongService } from '../song/songs.service';
 
 @Component({
   selector: 'app-quick-search',
@@ -6,28 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quick-search.component.css']
 })
 export class QuickSearchComponent implements OnInit {
-	searchResults = [{
-		name: 'test',
-		genre: 'BLUES', 
-		song_path: 'http://none',
-		image_path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyoxytt4ywnKXQJdgeZG5Z41Umk92jcWMGB7SGwg_rnO8KKYYP',
-		release_date: 'http://none',
-		artists:['some_object_id'],
-		num_of_times_liked: 1
-	},
-	{
-		name: 'test2',
-		genre: 'CLASSIC', 
-		song_path: 'http://none',
-		image_path: 'https://i.kinja-img.com/gawker-media/image/upload/s--_40CUoZX--/c_fill,f_auto,fl_progressive,g_center,h_264,q_80,w_470/i3x37tdozf9bpui6ssec.jpg',
-		release_date: 'http://none',
-		artists:['some_object_id'],
-		num_of_times_liked: 2
-	}];
+	// TODO remove later
+	searchResults = []
+	constructor(private songService: SongService) { }
 
-  constructor() { }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
 
+	onEnter(searchQuery){
+		this.songService.quickSearch(searchQuery, this.onSearchResults.bind(this));
+	}
+
+	onSearchResults(results) {
+		this.searchResults = results;
+	}
 }
